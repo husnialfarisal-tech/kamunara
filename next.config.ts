efd8ede99787570b323b57abe505cd2de522674d
jossmulalinda <josstugass@gmail.com>
2026-01-24 16:48:07 +0900

    1

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   
-  // PENTING: Agar saat build, dia bikin folder (home/index.html)
-  // Hasilnya URL di browser jadi bersih: kamunara.com/home/ (tanpa .html)
   trailingSlash: true,
 
   images: {
